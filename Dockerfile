39520c5d6046a04e54c46a2b9cfd8d4a7e12f5a2
Mateus Soares <mateus.silva@pravaler.com.br>
2020-05-19 22:43:51 -0300

    socket.io server enabled

--- Dockerfile
+++ Dockerfile
@@ -12,4 +12,4 @@ COPY . .
 RUN poetry config virtualenvs.create false
 RUN poetry install
 
-CMD flask run --host 0.0.0.0
+CMD flask run --host '0.0.0.0'
--- app.py
+++ app.py
@@ -1,16 +1,25 @@
-from flask import Flask, jsonify, request
+import eventlet 
+from flask import Flask, jsonify
+from flask_socketio import SocketIO
 from flask_pymongo import PyMongo
 
+eventlet.monkey_patch()
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
 app.config["MONGO_URI"] = "mongodb://root:docker@mongo:27017/todos?authSource=admin"
 mongo = PyMongo(app)
+
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route("/healthcheck")
 def healthcheck():
     data = {'message': 'The server is running! (sneake-chat)'}
     return jsonify(data)
 
-# start the development server using the run() method
-if __name__ == "__main__":
-    app.run()
-    
+@socketio.on('message')
+def handle_message(message):
+    print('received message: ' + str(message))
+
+if __name__ == '__main__':
+    socketio.run(app)
