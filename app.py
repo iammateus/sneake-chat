bb7cdd34c8e335eb91cd91d7d7bb1e215131b3af
Mateus Soares <mateus.silva@pravaler.com.br>
2020-05-19 23:41:01 -0300

    Join room feature created

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import eventlet 
 from flask import Flask, jsonify
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, join_room, leave_room
 from flask_pymongo import PyMongo
 
 eventlet.monkey_patch()
@@ -20,6 +20,12 @@ def healthcheck():
 @socketio.on('message')
 def handle_message(message):
     print('received message: ' + str(message))
+
+@socketio.on('join')
+def handleJoin(data):
+    room = data['room']
+    join_room(room)
+    socketio.emit('joined', 'Someone joined the room', room=room)
 
 if __name__ == '__main__':
     socketio.run(app)
